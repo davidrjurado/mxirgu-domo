65bd4710535d2f4f030294b20c112c7cf49496e0
davidrjurado <david.rjurado@gmail.com>
2018-09-15 18:35:53 +0200

    NA

---
+++ mxirgu/src/main/java/com/mxirgu/domo/config/WebSecurityConfig.java
@@ -0,0 +1,73 @@
+package com.mxirgu.domo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.mxirgu.domo.service.UserDetailsServiceImp;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackages = { "com.mxirgu.domo.service" })
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	UserDetailsServiceImp userDetailsServiceImp;
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return userDetailsServiceImp;
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.csrf().disable();
+
+		// The pages does not require login
+		http.authorizeRequests().antMatchers("/", "/welcome", "/login", "/logout").permitAll();
+
+		// /userInfo page requires login as USER or ADMIN.
+		// If no login, it will redirect to /login page.
+		http.authorizeRequests().antMatchers("/userInfo").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
+
+		// For ADMIN only.
+		http.authorizeRequests().antMatchers("/admin").access("hasRole('ROLE_ADMIN')");
+
+		// When the user has logged in as XX.
+		// But access a page that requires role YY,
+		// AccessDeniedException will throw.
+		http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
+
+		// Config for Login Form
+		http.authorizeRequests().and().formLogin()//
+				// Submit URL of login page.
+				.loginProcessingUrl("/j_spring_security_check") // Submit URL
+				.loginPage("/login")//
+				.defaultSuccessUrl("/userInfo")//
+				.failureUrl("/login?error=true")//
+				.usernameParameter("username")//
+				.passwordParameter("password")
+				// Config for Logout Page
+				.and().logout().logoutUrl("/logout").logoutSuccessUrl("/logoutSuccessful");
+
+	}
+
+}
---
+++ mxirgu/src/main/java/com/mxirgu/domo/controller/password.java
@@ -0,0 +1,19 @@
+package com.mxirgu.domo.controller;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class password {
+
+  public static void main(String[] args) {
+
+	int i = 0;
+	while (i < 10) {
+		String password = "123456";
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String hashedPassword = passwordEncoder.encode(password);
+
+		System.out.println(hashedPassword);
+		i++;
+	}
+  }
+  }
